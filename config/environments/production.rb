826bcaa69a70b4a7baf8c6ab1d4883676e291b54
Daniel Zhang <dyz2@duke.edu>
2014-07-21 09:12:44 -0700

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.force_ssl = true
   # Code is not reloaded between requests.
+
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
